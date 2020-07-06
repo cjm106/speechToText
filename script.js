8cc39c27d27717a1b45089f3da4a40da50ee065b
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-07-06 17:24:27 +0100

    refactor

--- script.js
+++ script.js
@@ -1,44 +1,28 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
-
 var recognition = new SpeechRecognition();
-
 var Textbox = $('#textbox');
-var instructions = $('instructions');
-
 var Content = '';
 
 recognition.continuous = true;
 
 recognition.onresult = function(event) {
-
   var current = event.resultIndex;
-
   var transcript = event.results[current][0].transcript;
-
     Content += transcript;
     Textbox.val(Content);
-
 };
-
-recognition.onstart = function() {
-  instructions.text('Voice recognition is ON.');
-}
-
-recognition.onspeechend = function() {
-  instructions.text('No activity.');
-}
-
-recognition.onerror = function(event) {
-  if(event.error == 'no-speech') {
-    instructions.text('Try again.');
-  }
-}
 
 $('#start-btn').on('click', function(e) {
   if (Content.length) {
     Content += ' ';
   }
+  console.log('Start button pressed')
   recognition.start();
+});
+
+$('#stop-btn').on('click', function(e) {
+  console.log('Stop button pressed')
+  recognition.stop();
 });
 
 Textbox.on('input', function() {
