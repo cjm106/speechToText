65e94102e39068b4d232d1d154254465acf04526
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-07-07 16:25:01 +0100

    refactor + added language selector

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,39 @@
-var SpeechRecognition = window.webkitSpeechRecognition;
-var recognition = new SpeechRecognition();
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+
 var Textbox = $('#textbox');
 var Content = '';
 
+var recognition = new SpeechRecognition();
+var selectedLanguage = 'en-GB'
 recognition.continuous = true;
 
 recognition.onresult = function(event) {
   var current = event.resultIndex;
   var transcript = event.results[current][0].transcript;
-    Content += transcript;
-    Textbox.val(Content);
+  Content += transcript;
+  Textbox.val(Content);
 };
+
+$('#lang').on('change', function(e) {
+  selectedLanguage = $('#lang').find(':selected').attr('value');
+});
 
 $('#start-btn').on('click', function(e) {
   if (Content.length) {
     Content += ' ';
   }
-  console.log('Start button pressed')
+  recognition.lang = selectedLanguage
   recognition.start();
+  $('#stop-btn').attr("disabled", false)
+  $('#lang').attr("disabled", true)
+  $('#start-btn').attr("disabled", true)
 });
 
 $('#stop-btn').on('click', function(e) {
-  console.log('Stop button pressed')
   recognition.stop();
+  $('#stop-btn').attr("disabled", true)
+  $('#lang').attr("disabled", false)
+  $('#start-btn').attr("disabled", false)
 });
 
 Textbox.on('input', function() {
