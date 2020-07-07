dd4f3d878f7911d54a4bbdeece288f907ec5a239
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-07-07 22:44:06 +0100

    added the ability to clear the transcription

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,15 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 
 var Textbox = $('#textbox');
 var Content = '';
-$('#stop-btn').attr("disabled", true)
-$('#copy-btn').attr("disabled", true)
+
 var recognition = new SpeechRecognition();
-var selectedLanguage = 'en-GB'
+var selectedLanguage = 'en-GB';
 recognition.continuous = true;
+
+$('#stop-btn').attr('disabled', true);
+$('#copy-btn').attr('disabled', true);
+$('#clear-btn').attr('disabled', true);
 
 recognition.onresult = function(event) {
   var current = event.resultIndex;
@@ -16,39 +19,61 @@ recognition.onresult = function(event) {
 };
 
 $('#lang').on('change', function(e) {
+
   selectedLanguage = $('#lang').find(':selected').attr('value');
-  console.log(selectedLanguage)
+
 });
 
 $('#start-btn').on('click', function(e) {
+
   if (Content.length) {
     Content += ' ';
   }
+
   recognition.lang = selectedLanguage
   recognition.start();
-  $('#stop-btn').attr("disabled", false)
-  $('#start-btn').attr("disabled", true)
+
+  $('#stop-btn').attr('disabled', false);
+  $('#clear-btn').attr('disabled', true);
+  $('#start-btn').attr('disabled', true);
+  $('#copy-btn').attr('disabled', true);
+  
 });
 
 $('#stop-btn').on('click', function(e) {
+
   recognition.stop();
-  $('#stop-btn').attr("disabled", true)
-  $('#start-btn').attr("disabled", false)
-  $('#copy-btn').attr("disabled", false)
-  console.log(Content)
+
+  $('#stop-btn').attr('disabled', true);
+  $('#start-btn').attr('disabled', false);
+  $('#copy-btn').attr('disabled', false);
+  $('#clear-btn').attr('disabled', false);
+
 });
 
 Textbox.on('input', function() {
+
   Content = $(this).val();
-})
+
+});
 
 $('#copy-btn').on('click', function(e) {
 
-  var copyText = document.createElement("textarea");
+  var copyText = document.createElement('textarea');
+
   document.body.appendChild(copyText);
   copyText.value = Content;
   copyText.select();
-  document.execCommand("copy");
+  document.execCommand('copy');
   document.body.removeChild(copyText);
+
+});
+
+$('#clear-btn').on('click', function(e) {
+
+  Textbox.val("")
+
+  $('#clear-btn').attr('disabled', true);
+  $('#copy-btn').attr('disabled', true);
 
 });
